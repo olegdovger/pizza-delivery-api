0843575b49e6581963a1d8f19b095496f08d43f9
Oleg Dovger <oleg.a.dovger@gmail.com>
2018-08-12 14:09:53 +0300

    Add lib/dao.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var server = require('./lib/server')
+
+// Declare the app
+var app = {}
+
+// Init function
+app.init = function () {
+  // Start the server
+  server.init()
+}
+
+// Self executing
+app.init()
+
+// Export the app
+module.exports = app
